ac6725d5f019ef9d0d1a175dbff39e7197b67bd9
Thibaut Horel <thibaut.horel@gmail.com>
2020-12-08 12:58:21 -0500

    Merge history WIP

---
+++ src/link_history.py
@@ -0,0 +1,55 @@
+from set_matcher import SetMatcher
+from utils import csv_read, flatten_stars
+
+
+def f1(officer, m):
+    if (
+        len(s := set(u["uid"] for o in officer if "middle_initial" in o for u in m[o]))
+        == 1
+    ):
+        return s.pop()
+
+
+f1.key = [
+    "first_name",
+    "last_name",
+    "race",
+    "gender",
+    "appointment_date",
+    "middle_initial",
+]
+
+
+def f2(officer, m):
+    s = set(u["uid"] for o in officer if "star1" in o for u in m[flatten_stars(o)])
+    if len(s) == 1:
+        return s.pop()
+
+
+f2.key = ["stars", "appointment_date"]
+# f2.debug = True
+
+
+def f3(officer, m):
+    if len(s := set(u["uid"] for o in officer if "birthyear" in o for u in m[o])) == 1:
+        return s.pop()
+
+
+f3.key = ["last_name", "appointment_date", "birthyear"]
+# f4.debug = True
+
+f4 = lambda o, m: f3(o, m)
+f4.key = ["first_name", "last_name", "birthyear"]
+# f4.debug = True
+
+f5 = lambda o, m: f3(o, m)
+f5.key = ["first_name", "appointment_date", "birthyear"]
+# f5.debug = True
+
+
+def link(officers):
+    m = SetMatcher()
+    for officer_profile in csv_read("linked/profiles.csv"):
+        m.add(flatten_stars(officer_profile))
+    linked, unlinked = m.match(officers, [f1, f2, f1, f3, f4, f5])
+    return m, linked, unlinked
--- src/merge_history.py
+++ src/merge_history.py
@@ -4,6 +4,19 @@ from collections import defaultdict
 from uuid import uuid4
 from itertools import chain
 from datetime import date
+from csv import DictWriter
+
+
+def before(old, new):
+    h1, h2 = old["history"], new["history"]
+    if (n := len(h1)) > len(h2):
+        return False
+    else:
+        return all(
+            h2[i] == h1[i]
+            or (i == (n - 1) and h2[i][:2] == h1[i][:2] and h1[i][2] == "")
+            for i in range(n)
+        )
 
 
 def comp_age(officer1, officer2):
@@ -13,14 +26,14 @@ def comp_age(officer1, officer2):
     return int(officer1["birthyear"]) in [birthyear, birthyear - 1]
 
 
-def flatten_history(changes, officer_attributes):
-    hist_key = ["unit_number", "start_date", "end_date"]
+def flatten_history(changes, id_attributes):
+    hist_key = ["unit_no", "start_date", "end_date"]
     officers = defaultdict(list)
     for change in changes:
-        officer_key = tuple(change[k] for k in officer_attributes)
-        officers[officer_key].append(tuple(change[k] for k in hist_key))
-    for o, history in officers.items():
-        officer = dict(zip(officer_attributes, o))
+        key = tuple(change[k] for k in id_attributes)
+        officers[key].append(tuple(change[k] for k in hist_key))
+    for key, history in officers.items():
+        officer = dict(zip(id_attributes, key))
         officer["history"] = sorted(history, key=lambda e: e[1])
         yield officer
 
@@ -49,25 +62,8 @@ def f2(officer, m):
 
 
 f2.key = ["first_name", "gender", "race", "appointment_date"]
-
-
-def f3(officer, m):
-    return f2(officer, m)
-
-
+f3 = lambda o, m: f2(o, m)
 f3.key = ["last_name", "gender", "race", "appointment_date"]
-
-
-def before(old, new):
-    h1, h2 = old["history"], new["history"]
-    if (n := len(h1)) > len(h2):
-        return False
-    else:
-        return all(
-            h2[i] == h1[i]
-            or (i == (n - 1) and h2[i][:2] == h1[i][:2] and h1[i][2] == "")
-            for i in range(n)
-        )
 
 
 if __name__ == "__main__":
@@ -77,7 +73,7 @@ if __name__ == "__main__":
     officers = flatten_history(csv_read("parsed/16-1105.csv"), key)
     m = Matcher(officers)
 
-    key = [
+    nkey = [
         "last_name",
         "first_name",
         "gender",
@@ -86,9 +82,52 @@ if __name__ == "__main__":
         "middle_initial",
         "birthyear",
     ]
-    officers = flatten_history(csv_read("parsed/P0-52262.csv"), key)
+    officers = flatten_history(csv_read("parsed/P0-52262.csv"), nkey)
     linked, unlinked = m.match(officers, [f1, f2, f3])
+
+    officers = defaultdict(list)
+    history = dict()
+    for o in chain(m, m.removed(), linked):
+        officers[o["uid"]].append(o)
+        del o["uid"]
     for o in unlinked:
-        print(o)
-    r1 = {o["uid"]: o for o in chain(m, m._removed)}
-    r2 = {o["uid"]: o for o in linked if o["uid"] in r1}
+        o["uid"] = uuid4()
+        officers[o["uid"]].append(o)
+        del o["uid"]
+
+    from link_history import link
+
+    m, linked, unlinked = link(officers.values())
+    fieldnames = [
+        "last_name",
+        "first_name",
+        "middle_initial",
+        "gender",
+        "race",
+        "birthyear",
+        "age",
+        "status",
+        "appointment_date",
+        "position_no",
+        "position_description",
+        "unit_no",
+        "unit_description",
+        "resignation_date",
+    ]
+    fieldnames += ["star" + str(i) for i in range(1, 12)]
+    fieldnames += ["star", "sworn", "unid_id", "unit_detail", "uid", "source"]
+
+    with open("linked/profiles.csv", "w") as pf, open("linked/history.csv", "w") as hf:
+        pw = DictWriter(pf, fieldnames=fieldnames, extrasaction="ignore")
+        hw = DictWriter(
+            hf, fieldnames=["uid", "start_date", "end_date"], extrasaction="ignore"
+        )
+        pw.writeheader()
+        hw.writeheader()
+        for l in unlinked:
+            uid = uuid4()
+            for u in l:
+                u["uid"] = uid
+        for o in chain(linked, unlinked, m._dict.values(), m.removed()):
+            for u in o:
+                pw.writerow(u)
