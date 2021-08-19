78804c3e9f53a0cd535974d96019ec2607e6c84f
Thibaut Horel <thibaut.horel@gmail.com>
2021-08-18 21:14:18 -0400

    Fix #20

--- src/parse.py
+++ src/parse.py
@@ -2,11 +2,16 @@ import sys
 import os.path
 import utils
 from datasets import datasets
+import os
 
 if __name__ == "__main__":
     target = sys.argv[1]
     root, _ = os.path.splitext(os.path.basename(target))
     dataset = datasets[root]
-    utils.process(
-        dataset["rows"](sys.argv[2]), target, dataset["fields"], dataset["rules"]
-    )
+    try:
+        utils.process(
+                dataset["rows"](sys.argv[2]), target, dataset["fields"], dataset["rules"]
+                )
+    except:
+        os.remove(target)
+        raise
