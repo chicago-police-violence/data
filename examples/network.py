b8702ccc6bd1665ef16a654d0ed94b31de3f7097
Thibaut Horel <thibaut.horel@gmail.com>
2021-08-23 14:54:19 -0400

    Add couple of comments to example file (resolves partially #6)

--- examples/network.py
+++ examples/network.py
@@ -7,16 +7,19 @@ import os.path as op
 DATA_DIR = "../final/"
 
 def csv_read(filename):
-    with open(filename) as fh:
+    with open(op.join(DATA_DIR,filename)) as fh:
         yield from csv.DictReader(fh)
 
 def build_complaint_graph():
-    complaints_officers = defaultdict(set) # maps a complain_no to set of officers
-    for row in csv_read(op.join(DATA_DIR, "complaints_officers.csv")):
+    # build a dictionary mapping a complaint_no to set of UIDs of officers
+    # listed in the complaint
+    complaints_officers = defaultdict(set)
+    for row in csv_read("complaints_officers.csv"):
         complaints_officers[row["complaint_no"]].add(row["uid"])
 
     g = nx.Graph()
     for complaint_no, officers in complaints_officers.items():
+        # add an edge for each pair of officers appearing in a given complaint
         for pair in combinations(officers,2):
             g.add_edge(*pair)
     return g
@@ -26,6 +29,6 @@ if __name__ == "__main__":
     print(f"Complaints graph: {len(g)} officers, {g.size()} edges")
 
     # load roster and print information of highest degree node
-    roster = {row["uid"]: row for row in csv_read(op.join(DATA_DIR, "roster.csv"))}
+    roster = {row["uid"]: row for row in csv_read("roster.csv")}
     node, degree = max(g.degree(), key=lambda x: x[1])
     print(f"Max degree node: {roster[node]}")
