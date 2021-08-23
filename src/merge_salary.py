021d02a107571c61b0d110eda593929a03da13db
Trevor Campbell <trevor.d.campbell@gmail.com>
2021-08-23 15:43:38 -0700

    done matching salary

--- src/merge_salary.py
+++ src/merge_salary.py
@@ -13,25 +13,50 @@ from datetime import datetime
 
 # check if a record matches a list of other records
 # it does not match if there is a conflict in Age or MI (if they exist)
+# within these groupings, we check last name, middle init, age, officer_date, appointment_date, present_posn_start_date
+# if apt_date is missing, fill in with officer_date
+# compute birthyear_apt and birthyear_off 
+# if last name agrees: MI must agree or be missing, one of apt date / officer date must agree (cross links possible), and birthyear must be within 1y (cross link possible)
+# if last name doesn't agree: MI must agree or be missing, apt date and birthyear_apt must agree
+
 def record_matches_list(rec, li):
     for lr in li:
+        # if middle initial exists in both, they must match
         if rec['middle_initial'] != '' and lr['middle_initial'] != '' and rec['middle_initial'] != lr['middle_initial']:
             return False
-        if rec['age_appointment'] != '' and lr['age_appointment'] != '' and abs(int(rec['age_appointment']) - int(lr['age_appointment'])) > 1:
-            return False
+        # birthyear is a bit unreliable (it can often jump by about 10 years, especially in the records for 2016,2017...)
+        # so as long as we get a strong match on other attributes, birthyear can vary; otherwise, it can't (only 1y allowed)
+
+        # option 1: MI exists and matches, appointment date and officer date both exist & match (not necessarily different)
+        # option 2: MI soft matches (might be missing), appointment date and officer date both match and are different (unique info like this implies good match)
+        # then BY not necessary to check
+        # otherwise, BY must be within 1Y
+        mi_hard_match = rec['middle_initial'] != '' and rec['middle_initial'] == lr['middle_initial']
+        mi_soft_match = (rec['middle_initial'] == '') or (lr['middle_initial'] == '') or (rec['middle_initial'] == lr['middle_initial'])
+        apt_hard_match = rec['appointment_date'] != '' and rec['appointment_date'] == lr['appointment_date']
+        off_hard_match = rec['officer_date'] != '' and rec['officer_date'] == lr['officer_date']
+        apt_off_diff = rec['officer_date'] != rec['appointment_date']
+        if not ((mi_hard_match and apt_hard_match and off_hard_match) or (mi_soft_match and apt_hard_match and off_hard_match and apt_off_diff)):
+            if rec['birthyear'] != '' and lr['birthyear'] != '' and abs(int(rec['birthyear']) - int(lr['birthyear'])) > 1:
+                return False
+    #passed all checks, so match
     return True
 
 def flatten_salary(records, id_attributes):
-    
-    # match on name and start date (there will be multiple officers in each group, this is very coarse)
-    # if start date as city emp is missing, fill it in with officer date
-    flatten_attributes = ['first_name', 'last_name', 'appointment_date']
+
+    # match on first name and gender (there will of course be multiple officers in each group, this is very coarse)
+    flatten_attributes = ['last_name', 'first_name', 'appointment_date']
     officers = defaultdict(list)
     for record in records:
-        key = tuple((record[k] if (record[k] != '' or k != 'appointment_date') else record['officer_date']) for k in flatten_attributes)
+        # fill in appointment date with officer date if apt date is missing
+        if record['appointment_date'] == '':
+            record['appointment_date'] = record['officer_date']
+        # compute birthyear (up to 1 year)
+        record['birthyear'] = '' if (record['appointment_date'] == '' or record['age_hire'] == '') else str(datetime.strptime(record['appointment_date'], '%Y-%m-%d').year - int(record['age_hire']))
+        key = tuple(record[k] for k in flatten_attributes)
         officers[key].append(record)
 
-    # now within these groupings, consider a match if (MI nonempty + matches) or if age is within 1 year
+    # now we split these groupings by using more fine-grained rules
     _officers = []
     for key, recs in officers.items():
         unq_officers = []
@@ -44,6 +69,12 @@ def flatten_salary(records, id_attributes):
                     break
             if not found:
                 unq_officers.append([rec])
+        # sometimes this splits an officer into two. We can fix the majority of these by seeing whether there are mutually disjoint salary years
+        if len(unq_officers) == 2:
+            years0 = [rec['year'] for rec in unq_officers[0]]
+            years1 = [rec['year'] for rec in unq_officers[1]]
+            if len(years0) + len(years1) == len(set(years0 + years1)):
+                unq_officers = [unq_officers[0]+unq_officers[1]]
         _officers.extend(unq_officers)
     officers = _officers
             
@@ -54,7 +85,8 @@ def flatten_salary(records, id_attributes):
             year_posns[rec['year']].append(rec['position_description'])
         for year, posns in year_posns.items():
             if len(set(posns)) != len(posns):
-                print(f"\nWarning: single officer has multiple records for same posn in same year\n{recs}\n\n")
+                print(f"Warning: single officer has multiple records for same posn in same year\n{recs[0]['last_name'], recs[0]['first_name'], recs[0]['middle_initial'], recs[0]['birthyear']}")
+                break
         
     # there are some officers with multiple records in one year
     # this happens when their position changes
@@ -67,15 +99,19 @@ def flatten_salary(records, id_attributes):
             salary_history[record['year']].append({k : v for k, v in record.items() if k not in id_attributes})
         for year in salary_history:
             salary_history[year] = sorted(salary_history[year], key=lambda ll : ll['present_posn_start_date'])
-        off = {k : v for k, v in record.items() if k in id_attributes}
+        off = {}
         off['salary_history'] = salary_history
+        for attr in id_attributes + ['birthyear']:
+            attrvals = set([rec[attr] for rec in officer])
+            attrvals.discard('')
+            off[attr] = '' if len(attrvals) == 0 else list(attrvals)[0]
         yield off
 
 # TODO handle the multiple matches case properly
 # match on full name and apt date
 def f1(officer, m):
     if len(officers := m[officer]) >= 1:
-        unique_uids = set([officer['uid'] for officer in officers])
+        unique_uids = set([off['uid'] for off in officers])
         if len(unique_uids) > 1:
             print(f"Warning: matched to multiple officers:\n Officers: {set([(off['first_name'], off['last_name'], off['uid']) for off in officers])}")
         return officers[0]["uid"]
@@ -83,91 +119,84 @@ def f1(officer, m):
 f1.key = ["first_name", "last_name", "middle_initial", "appointment_date"]
 
 
-# match on name and apt date (but allow either age or MI match)
+# match on name and apt date (but force MI to match, or MI to soft match and age to match)
 def f2(officer, m):
     if len(officers := m[officer]) >= 1:
-        unique_uids = set([officer['uid'] for officer in officers])
+        unique_uids = set([off['uid'] for off in officers])
         if len(unique_uids) > 1:
             print(f"Warning: matched to multiple officers:\n Officers: {set([(off['first_name'], off['last_name'], off['uid']) for off in officers])}")
         off1 = officer
         mi1 = off1['middle_initial']
-        by1 = None
-        try:
-            by1 = datetime.strptime(off1['appointment_date'], '%Y-%m-%d').year - int(off1['age_appointment'])
-        except:
-            pass
-        off2 = officers[0]
-        mi2 = off2['middle_initial']
-        by2 = None
-        for _o2 in officers:
-            try:
-                by2 = int(_o2['birthyear'])
-            except:
-                pass
-        if mi1.strip() == mi2.strip():
-            return off2["uid"]
-        if by1 and by2 and abs(by1-by2) < 2:
-            return off2["uid"]
+        by1 = off1['birthyear']
+        for off2 in officers:
+            mi2 = off2['middle_initial']
+            by2 = off2['birthyear']
+            if mi1 != '' and mi1.strip() == mi2.strip():
+                return off2["uid"]
+            if by1 != '' and by2 != '' and abs(int(by1)-int(by2)) < 2 and (mi1 == '' or mi2 == '' or mi1.strip() == mi2.strip()):
+                return off2["uid"]
 
 f2.key = ["first_name", "last_name", "appointment_date"]
 
-# match on just name (but pick the entry that matches the most of apt date, MI, and age)
-# there will be quite a few muti-uid matches here
+
+def normalize_name(name):
+    # first strip whitespace
+    name = name.strip()
+    # first remove suffix
+    suffixes = [' JR', ' SR', ' II', ' III', ' IV']
+    namesuff = ''
+    for suff in suffixes:
+        if name[-len(suff):] == suff:
+            namesuff = suff
+            name = name[:-len(suff)]
+            break
+    # normalize the main name
+    name = ''.join([ch for ch in name if ch.isalpha()])
+    return name, namesuff
+
+# match on first name and appointment date, then normalize lastname (because these two DBs differ), then apply f2
 def f3(officer, m):
-    if len(officers := m[officer]) >= 1:
+    ln, lsuff = normalize_name(officer['last_name'])
+    submatches = []
+    for off in m[officer]:
+        # extract normalized last name
+        ln2, lsuff2 = normalize_name(off['last_name'])
+        # if suffix soft matches, and last name hard matches, add this officer to the submatch list
+        # otherwise (e.g. if one is JR and the other is SR) no match
+        if ln != '' and ln == ln2 and (lsuff == '' or lsuff2 == '' or lsuff == lsuff2):
+            submatches.append(off)
+    # now essentially run f2
+    if len(submatches) >= 1:
+        unique_uids = set([off['uid'] for off in submatches])
+        if len(unique_uids) > 1:
+            print(f"Warning: matched to multiple officers:\n Officers: {set([(off['first_name'], off['last_name'], off['uid']) for off in submatches])}")
         off1 = officer
         mi1 = off1['middle_initial']
-        ad1 = off1['appointment_date'] if off1['appointment_date'] != '' else off1['officer_date']
-        by1 = None
-        try:
-            by1 = datetime.strptime(off1['appointment_date'], '%Y-%m-%d').year - int(off1['age_appointment'])
-        except:
-            pass
-        maxscore = 0
-        bestoff2 = None
-        for off2 in officers:
+        by1 = off1['birthyear']
+        for off2 in submatches:
             mi2 = off2['middle_initial']
-            by2 = None
-            for _o2 in officers:
-                try:
-                    by2 = int(_o2['birthyear'])
-                except:
-                    pass
-            ad2 = off2['appointment_date']
-            score = (mi1.strip() == mi2.strip()) + (ad1 == ad2)
-            if by1 and by2 and abs(by2-by1) < 2:
-                score += 1
-            if score > maxscore:
-                maxscore = score
-                bestoff2 = off2
-        if bestoff2:
-            #print(f"\n\nMATCH3:\n{officer}\n{bestoff2}\n\n")
-            return bestoff2['uid']
-f3.key = ["first_name", "last_name"]
+            by2 = off2['birthyear']
+            if mi1 != '' and mi1.strip() == mi2.strip():
+                return off2["uid"]
+            if by1 != '' and by2 != '' and abs(int(by1)-int(by2)) < 2 and (mi1 == '' or mi2 == '' or mi1.strip() == mi2.strip()):
+                return off2["uid"]
 
+f3.key = ["first_name", "appointment_date"]
 
-# match on just one name + apt date, and match as long as there is only one UID so far in the cleaning
-# (there are a bunch of misspellings of names in the HR data...)
 def f4(officer, m):
     if len(officers := m[officer]) >= 1:
-        unique_uids = set([officer['uid'] for officer in officers])
-        if len(unique_uids) == 1:
-            print(f"\n\nMATCH3:\n{officer}\n{officers[0]}\n\n")
-            if officer['middle_initial'] != '' and officers[0]['middle_initial'] != '' and officer['middle_initial'] != officers[0]['middle_initial']:
-                print(f"\n\nBADMATCH3:\n{officer}\n{officers[0]}\n\n")
-            return officers[0]['uid']
-f4.key = ["last_name", "appointment_date"]
-
-#
-#def f4(officer, m):
-#    if len(officers := m[officer]) >= 1:
-#        unique_uids = set([officer['uid'] for officer in officers])
-#        if len(unique_uids) == 1:
-#            print(f"\n\nMATCH4:\n{officer}\n{officers[0]}\n\n")
-#            if officer['middle_initial'] != '' and officers[0]['middle_initial'] != '' and officer['middle_initial'] != officers[0]['middle_initial']:
-#                print(f"\n\nBADMATCH4:\n{officer}\n{officers[0]}\n\n")
-#            return officers[0]['uid']
-#f4.key = ["first_name", "appointment_date"]
+        unique_uids = set([off['uid'] for off in officers])
+        if len(unique_uids) > 1:
+            print(f"Warning: matched to multiple officers:\n Officers: {set([(off['first_name'], off['last_name'], off['uid']) for off in officers])}")
+        off1 = officer
+        mi1 = off1['middle_initial']
+        by1 = off1['birthyear']
+        for off2 in officers:
+            mi2 = off2['middle_initial']
+            by2 = off2['birthyear']
+            if mi1 != '' and mi2 != '' and by1 != '' and by2 != '' and abs(int(by1) - int(by2)) < 2:
+                return off2["uid"]
+f4.key = ["first_name", "middle_initial", "appointment_date"]
 
 if __name__ == "__main__":
     from sys import argv
@@ -178,77 +207,8 @@ if __name__ == "__main__":
     salary_records = multi_csv_read(argv[3:6])
     salary_flat = flatten_salary(salary_records, datasets[basename]["id_fields"])
 
-
-    # code that merges entries using a more coarse matcher
-    # shows officers that get split up by the above very fine-grained flattening
-    # so that you can visually inspect splits to make sure they are sensible
-    # this code block is just for visually debugging the flattening, so commented out.
-
-    offs = defaultdict(list)
-    for ff in salary_flat:
-        key = (ff['last_name'], ff['first_name'], ff['appointment_date'])
-        offs[key].append(ff)
-
-    for key, off in offs.items():
-        # last_name,first_name,middle_initial,gender,age_appointment,position_description,officer_date,present_posn_start_date,appointment_date,salary,pay_grade,employee_status,year
-        key1 = (key[0], key[1], '')
-        if len(off) > 1 or (key1 != key and key1 in offs):
-            print()
-            print('KEY')
-            print(key)
-            print(f'IN {key}')
-            for li in off:
-                print('ITEM')
-                print((li['last_name'], li['first_name'], li['middle_initial'], li['gender'], li['appointment_date'], li['age_appointment'], li['officer_date']))
-            if key1 != key and key1 in offs:
-               print(f'IN {key1}')
-               for li in offs[key1]:
-                   print('ITEM')
-                   print((li['last_name'], li['first_name'], li['middle_initial'], li['gender'], li['appointment_date'], li['age_appointment'], li['officer_date']))
-            print()
-    quit()
-
-
-
-    offs = defaultdict(list)
-    for ff in salary_flat:
-        key = (ff['last_name'], ff['first_name'], ff['appointment_date'], ff['age_appointment'])
-        offs[key].append(ff)
-
-    for key, off in offs.items():
-        # last_name,first_name,middle_initial,gender,age_appointment,position_description,officer_date,present_posn_start_date,appointment_date,salary,pay_grade,employee_status,year
-        key1 = (key[0], key[1], '', '')
-        key2 = (key[0], key[1], key[2], '')
-        key3 = (key[0], key[1], '', key[3])
-        if len(off) > 1 or (key1 != key and key1 in offs) or (key2 != key and key2 in offs) or (key3 != key and key3 in offs):
-            print()
-            print('KEY')
-            print(key)
-            print(f'IN {key}')
-            for li in off:
-                print('ITEM')
-                print((li['last_name'], li['first_name'], li['middle_initial'], li['gender'], li['appointment_date'], li['age_appointment'], li['officer_date']))
-            if key1 != key and key1 in offs:
-               print(f'IN {key1}')
-               for li in offs[key1]:
-                   print('ITEM')
-                   print((li['last_name'], li['first_name'], li['middle_initial'], li['gender'], li['appointment_date'], li['age_appointment'], li['officer_date']))
-            if key2 != key and key2 in offs:
-               print(f'IN {key2}')
-               for li in offs[key2]:
-                   print('ITEM')
-                   print((li['last_name'], li['first_name'], li['middle_initial'], li['gender'], li['appointment_date'], li['age_appointment'], li['officer_date']))
-            if key3 != key and key3 in offs:
-               print(f'IN {key3}')
-               for li in offs[key3]:
-                   print('ITEM')
-                   print((li['last_name'], li['first_name'], li['middle_initial'], li['gender'], li['appointment_date'], li['age_appointment'], li['officer_date']))
-            print()
-    quit()
-
-
     m = Matcher(profiles)
-    linked, unlinked = m.match(salary_flat, [f1, f2, f3])
+    linked, unlinked = m.match(salary_flat, [f1, f2, f3, f4])
 
     profiles = sorted(
             m.unify(linked, unlinked, matchee_source='salary'),
